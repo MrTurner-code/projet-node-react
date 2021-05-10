8edb95fa380700f9af2323c2a6ab94865cdb322b
Sudrat Thomas <thomas.sudrat.pro@gmail.com>
2021-05-10 12:38:50 +0200

    no message

--- node_src/app.js
+++ node_src/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-let connect = require("./connection.js")
-let config = require("./config.js")
+let connect = require("./connection.js");
+let config = require("./config.js");
 
 app.get('/app', (req, res) => {
     res.send('hello world ma gueule !')
